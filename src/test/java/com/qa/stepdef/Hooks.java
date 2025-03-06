4db88ea4fd54560d69537cd58897f29b7fb4f9a2
Hoa Truong <minhanhoa.truong@cynergi.io>
2025-03-06 22:53:05 +0700

    Video option 2: videoURL is local file, attach type = text/html (continue)

--- src/test/java/com/qa/stepdef/Hooks.java
+++ src/test/java/com/qa/stepdef/Hooks.java
@@ -120,7 +120,8 @@ public class Hooks {
             }
 
             // 4. Use a relative path that will work after publication
-            String videoUrl = "../" + params.getPlatformName() + "_" + params.getDeviceName() + "/" + "Videos" + "/" + videoFileName;
+//            String videoUrl = "../" + params.getPlatformName() + "_" + params.getDeviceName() + "/" + "Videos" + "/" + videoFileName;
+            String videoUrl = reportDirFile + GlobalConstants.SEPARATOR + videoFileName;
             utils.log().info("videoUrl: " + videoUrl);
 
             // 5. Generate HTML with a proper video element
